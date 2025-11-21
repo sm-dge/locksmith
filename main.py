a347d6083c52b808f151a55fc5407fae178c4fca
James <j4m3sgs@gmail.com>
2025-11-21 11:46:39 +0000

    Enhance password generator functionality
    Updated password generation logic to avoid certain symbols and added a message box for clipboard confirmation.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from random import choice
 from string import digits, punctuation
+import pandas as pd
 
 pw = ''
 words = open("words.txt", "r").readlines()
@@ -15,34 +16,37 @@ def new_pass():
         word = choice(words).capitalize()
         pw += word
     random_number = choice(digits)
-    random_symbol = choice(punctuation)
+    while True:
+        random_symbol = choice(punctuation)
+        if random_symbol in {',', '(', ')', '[', ']', '{', '}'}:
+            continue
+        break
     pw += random_number
     pw += random_symbol
     pass_label.config(text='Your new password: ' + pw)
 
 def copy_to_clipboard():
-    root.clipboard_clear()
-    root.clipboard_append(pw)
-    root.update() 
-    copy_label.config(text='Copied to Clipboard!')
+    passgen.clipboard_clear()
+    passgen.clipboard_append(pw)
+    passgen.update()
+    messagebox.showinfo('Copy to Clipboard','Password copied!')
 
 def pass_gen():
     global pass_label
-    global copy_label
-    global root
-    root = Tk()
-    root.title('LockSmith')
-    root.geometry('300x150')
+    global passgen
+    passgen = Tk()
+    passgen.title('LockSmith PassGen')
+    passgen.geometry('350x150')
+    passgen.resizable(False, False)
 
-    ttk.Label(root, text="Password Generator", font=("Arial", 16)).pack()
-    ttk.Button(root, text="Generate Password", command=new_pass).pack()
-    pass_label = ttk.Label(root, text="")
+    ttk.Label(passgen, text="Password Generator", font=("Arial", 16)).pack()
+    ttk.Button(passgen, text="Generate Password", command=new_pass).pack()
+    pass_label = ttk.Label(passgen, text="")
     pass_label.pack()
-    ttk.Button(root, text="Copy to Clipboard", command=copy_to_clipboard).pack()
-    copy_label = ttk.Label(root, text="")
-    copy_label.pack()
-    ttk.Label(root, text='Test Ctrl+V:').pack(side=LEFT)
-    ttk.Entry(root, width=30).pack(side=LEFT)
-    root.mainloop()
+    ttk.Button(passgen, text="Copy to Clipboard", command=copy_to_clipboard).pack()
+    ttk.Button(passgen, text="Back", command=lambda: passgen.destroy()).pack()
+
+    passgen.mainloop()
 pass_gen()
+
 
